6130eb54b7696135c8b1ae01b221c21631115ebc
Cary Miller <cary.a.miller@gmail.com>
2019-03-22 11:29:23 -0700

    Add launchTo(dateString:) extension

--- DateKit/DateKitExt.swift
+++ DateKit/DateKitExt.swift
@@ -1,16 +1,51 @@
 //
 //  DateKitExt.swift
 //
-//  Created by Cary Miller on 2/15/18.
-//  Copyright © 2018 Cary Miller.
+//  Copyright © 2018 Cary Miller. (http://cmillerco.com/)
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 //
 
 import XCTest
 
 public extension XCUIElement {
-   func set(_ date: String) {
-      self.tap()
-      self.typeText(date)
-      self.typeText("\r")
-   }
+
+    func set(_ date: String) {
+        self.tap()
+        self.typeText(date)
+        self.typeText("\r")
+    }
+}
+
+public extension XCUIApplication {
+
+    func launchTo(_ dateString: String) {
+        sleep(2)
+
+        let date = self.textFields["DateKitUI"]
+        date.set(dateString)
+
+        XCUIDevice.shared.press(.home)
+        if self.state == .notRunning {
+            self.launch()
+        } else {
+            self.activate()
+        }
+    }
 }
